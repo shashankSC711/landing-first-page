1db229789a0dbe30a752860b5379334b6212df52
shashankSC711 <118930768+shashankSC711@users.noreply.github.com>
2024-01-21 13:08:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+Shery.imageEffect("#back",{style:1,debug:true,})
+
+
+
+// var h1s = document.querySelectorAll("h1");
+// var index = 0;
+// document.querySelector("#main").addEventListener("cick", function () {
+//                gsap.to(h1s[index], {
+//                 top: "-=100%",
+//                 ease: Expo.easeInout,
+//                 duration: 1
+//     });
+// });
